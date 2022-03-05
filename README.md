1f2d925920c077f32533d3227adce18476a8fad9
Maze Token <68662262+mazetoken@users.noreply.github.com>
2022-03-05 22:19:43 +0100

    Arena Checker update

--- README.md
+++ README.md
@@ -1,41 +1,39 @@
-## MAZE-ARENA NFT - Into the unknown - [MAZE Token](https://mazetoken.github.io)
+## MAZE-ARENA NFT - Into the unknown
 
-MAZE-ARENA NFT is a multi NFT contract (ERC1155) on Bitcoin Cash SmartBCH
+MAZE-ARENA NFT is an experimental multi-token contract (non-fungible and fungible tokens - ERC1155). It is not a final version.
 
 Contract address: 0x7c0C2f18aeF4B0cC84D9E397aE47867aC04AEA19
 
-![MAZE-ARENA NFT Collection](img/MazeArena512x411.jpg)
+MAZE-ARENA NFT is (the first ?) collection of ERC1155 NFTs on Bitcoin Cash SmartBCH. Metadata and images are stored on IPFS.
+
+Read about [ERC1155](https://eips.ethereum.org/EIPS/eip-1155)
+
+![MAZE-ARENA NFTs](checker/img/allarena.png)
 
 ```
-Token id	Level		Character	Initial token supply		
+Token id	Level		Character	Initial supply	Total supply		
 
-1	 	Legendary	Architect	20
-2 		Mystic		Wizard		40
-3 		Mystic		Visionary	60
-4 		Epic		Gearhead	80
-5 		Epic		Cyborg		100
-6		Epic		Hero		120
-7 		Solo		Spy		140
-8 		Solo		Warrior		160
-9 		Solo		Brawler		180
-10 		Solo		Savage		200
+1	 	Legendary	Architect	20		500
+2 		Mystic		Wizard		40		500
+3 		Mystic		Visionary	60		500
+4 		Epic		Gearhead	80		500
+5 		Epic		Cyborg		100		500
+6		Epic		Hero		120		500
+7 		Solo		Spy		140		500
+8 		Solo		Warrior		160		500
+9 		Solo		Brawler		180		500
+10 		Solo		Savage		200		500
 ```
 
-_MetaMask does not fully support ERC155 yet. You may see "Unable to load Balance". It is because "balanceOfBatch" is not implemented, I guess. You may not be able to send ERC1115 NFTs with MetaMask. You need to interact with a contract through another app or use ARENA Checker._
+_MetaMask does not fully support ERC155 yet. You may see "Unable to load Balance". You may not be able to send ERC115 NFTs with MetaMask. You need to interact with a contract through anoter app e.g. [ARENA NFT Checker](https://mazetoken.github.io/arena/checker).
 
-### MAZE-ARENA NFT Checker
-
-[ARENA Checker](https://mazetoken.github.io/arena/checker) - view and send your ERC1155 NFTs (desktop or mobile)
-
-[MAZE-ARENA NFT](https://oneclickdapp.com/koala-yes) - OneClickDapp - to interact with the contract, e.g.:
+You can also use [MAZE-ARENA](https://oneclickdapp.com/koala-yes) - OneClickDapp - to interact with the contract, for example:
 
 - to send one of MAZE-ARENA tokens open this oneclickdapp link above and connect your wallet (MetaMask). Next tap on Write tab, tap on safeTransferFrom and paste your address in the FROM filed, paste address you want to send token to in TO field, paste token id in the ID field, paste amount (eg. 1) and paste 0x0000000000000000000000000000000000000000000000000000000000000000 in the DATA field. Tap submit and approve transaction
 
-_If everything is fine images (and media) are uploaded to IPFS_
+_If everything is fine images (and media) are uploaded on IPFS_
 
 ### MetaMask settings:
-
-connect to SmartBCH network
 
 _import token_
 
@@ -53,17 +51,9 @@ ID: ... - paste Token id
 
 _You may need to tap "import" twice"_
 
-### Sample preview
-
-MetaMask app:
-
-![Architect](img/architect.png)
-
-Browser:
-
-![Architect-apng](img/architect-apng.png)
 
 ------------------------------------------------------------------------------------
+MAZE Tokens Underworld 2020-2022
 
 [Telegram](https://t.me/mazetokens)
 
---
+++ checker/functions.js
@@ -0,0 +1,97 @@
+    function viewNFT1() {
+    // if not connected reload page
+    if (!zombieMaster) location.reload(true);
+    var tokenId = $("#tokenId").val();
+    var contract1 = new web3.eth.Contract(abi, '0x7c0C2f18aeF4B0cC84D9E397aE47867aC04AEA19'); // MAZE-ARENA NFT
+    var content = "<br>NFT image and balance of address: ";
+    content += zombieMaster;
+    $("#lang").html(content);
+    var event = contract1.methods.balanceOf(zombieMaster, tokenId).call()
+        .then(function (result) {
+    var content = "<br>Your balance of this token ID is: ";
+        console.log(result);
+    content += JSON.stringify(result.toString());
+    content += "<br><img src=https://mazetoken.github.io/arena/img/"+tokenId+".png width=366 height=366><br>";
+    $("#lang").html(content);
+        });;
+    };
+
+    function viewNFT2() {
+    // if not connected reload page
+    if (!zombieMaster) location.reload(true);
+    var tokenId = $("#tokenId").val();
+    var contract2 = new web3.eth.Contract(abi, '0x8ebf51b3435F232ED08cE5Fb2491B6eA21826f94'); // Yoshi NFT Series
+    var content = "<br>NFT image and balance of address: ";
+    content += zombieMaster;
+    $("#lang").html(content);
+    var event = contract2.methods.balanceOf(zombieMaster, tokenId).call()
+        .then(function (result) {
+    var content = "<br>Your balance of this token ID is: ";
+        console.log(result);
+    content += JSON.stringify(result.toString());
+    content += "<br><img src=https://ipfs.io/ipfs/QmUkL8535iXAXfrUUWBE7iEyvjRiWGJYkNoPVVdugkAvEw/"+tokenId+".png width=366 height=366><br>";
+    $("#lang").html(content);
+        });;
+    };
+
+    function viewNFT3() {
+        // if not connected reload page
+        if (!zombieMaster) location.reload(true);
+        var tokenId = $("#tokenId").val();
+        var custom1 = $("#custom").val();
+        var custom2 = $("#customURI").val();
+        var contract3 = new web3.eth.Contract(abi, custom1); // Custom NFT
+        var content = "<br>NFT image and balance of address: ";
+        content += zombieMaster;
+        $("#lang").html(content);
+        var event = contract3.methods.balanceOf(zombieMaster, tokenId).call()
+            .then(function (result) {
+        var content = "<br>Your balance of this token ID is: ";
+            console.log(result);
+        content += JSON.stringify(result.toString());
+        content += "<br><img src="+custom2+""+tokenId+".png width=366<br>";
+        $("#lang").html(content);
+            });;
+        };
+
+    function sendNFT1() {
+    // if not connected reload page
+    if (!zombieMaster) location.reload(true);
+    var to = $("#to").val();
+    var tokenId = $("#tokenId").val();
+    var contract1 = new web3.eth.Contract(abi, '0x7c0C2f18aeF4B0cC84D9E397aE47867aC04AEA19'); // MAZE-ARENA NFT
+    var amount = $("#amount").val();
+    var data = '0x0000000000000000000000000000000000000000000000000000000000000000';
+    var content = "<br>Sending from address: ";
+    content += zombieMaster;
+    $("#lang").html(content);
+    var event = contract1.methods.safeTransferFrom(zombieMaster, to, tokenId, amount, data).send({ from: zombieMaster, gasPrice: 1047000000 })
+        .then(function (receipt) {
+    var content = "<br>Sending transaction...";
+        console.log(receipt);
+    var content = "<br>Transaction sent!";
+    content += JSON.stringify(receipt.transactionHash);
+    $("#lang").html(content);
+        });;
+    };
+
+    function sendNFT2() {
+    // if not connected reload page
+    if (!zombieMaster) location.reload(true);
+    var to = $("#to").val();
+    var tokenId = $("#tokenId").val();
+    var contract2 = new web3.eth.Contract(abi, '0x8ebf51b3435F232ED08cE5Fb2491B6eA21826f94'); // Yoshi NFT Series
+    var amount = $("#amount").val();
+    var data = '0x0000000000000000000000000000000000000000000000000000000000000000';
+    var content = "<br>Sending from address: ";
+    content += zombieMaster;
+    $("#lang").html(content);
+    var event = contract2.methods.safeTransferFrom(zombieMaster, to, tokenId, amount, data).send({ from: zombieMaster, gasPrice: 1047000000 })
+        .then(function (receipt) {
+    var content = "<br>Sending transaction...";
+        console.log(receipt);
+    var content = "<br>Transaction sent!";
+    content += JSON.stringify(receipt.transactionHash);
+    $("#lang").html(content);
+        });;
+    };
